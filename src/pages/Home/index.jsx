6a25d077efd75e88b1117d71499a20402f0125e8
dionyguape <dionyguape@gmail.com>
2023-07-12 22:23:23 -0300

    Criando o menu e o botão criar nota

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,8 @@
+import { FiPlus } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
 import { Header } from '../../components/Header'
+import { ButtonText } from "../../components/ButtonText";
 
 export function Home() {
   return(
@@ -12,7 +14,9 @@ export function Home() {
       <Header />
 
       <Menu>
-
+        <li><ButtonText title='Todos' isActive /></li>
+        <li><ButtonText title='React' /></li>
+        <li><ButtonText title='NodeJS' /></li>
       </Menu>
 
       <Search>
@@ -24,7 +28,8 @@ export function Home() {
       </Content>
 
       <NewNote>
-
+        <FiPlus />
+        Criar Nota
       </NewNote>
 
     </Container>
